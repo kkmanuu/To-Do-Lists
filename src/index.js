1a655be578506e82e289ef3ac304c31c98dd4807
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-06-14 16:55:48 +0300

    Fix Linters

--- src/index.js
+++ src/index.js
@@ -1,50 +1,90 @@
-import { container } from './taskClass.js';
+// eslint-disable-next-line import/named
+import './style.css';
+import add from './modules/add.js';
+import taskRemaining from './modules/taskFunctions.js';
+import {
+  addButton, taskInput, container, clearChecked,
+} from './modules/taskClass.js';
+import { save, retrieve } from './modules/storage.js';
 
-class Tasks {
-  constructor() {
-    this.tasks = [];
+taskInput.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    if (taskInput.value === '') {
+      e.preventDefault();
+    } else {
+      const task = add(e);
+      taskRemaining.add(task);
+      taskRemaining.init();
+      save();
+      taskRemaining.display();
+    }
   }
+});
 
-  init = () => {
-    container.innerHTML = '';
-  };
+addButton.addEventListener('click', (e) => {
+  if (taskInput.value === '') {
+    e.preventDefault();
+  } else {
+    const task = add(e);
+    taskRemaining.add(task);
+    taskRemaining.init();
+    save();
+    taskRemaining.display();
+  }
+});
 
-  add = (task) => {
-    this.tasks.push(task);
-  };
+container.addEventListener('keypress', (e) => {
+  if (e.target.className === 'task-to-be-done' && e.key === 'Enter') {
+    if (e.target.textContent) {
+      e.preventDefault();
+      taskRemaining.update(e.target.textContent, e.target.parentElement.id);
+      save();
+    } else {
+      e.preventDefault();
+    }
+  }
+});
 
-  update = (desc, id) => {
-    this.tasks[id].description = desc;
-  };
+container.addEventListener('change', (e) => {
+  let desc = taskRemaining.tasks[e.target.parentElement.id].description;
+  if (e.target.type === 'checkbox') {
+    if (e.target.checked) {
+      taskRemaining.tasks[e.target.parentElement.id].completed = true;
+      e.target.nextElementSibling.innerHTML = `<strike>${desc}</strike>`;
+      taskRemaining.tasks[e.target.parentElement.id].description = `<strike>${desc}</strike>`;
+      save();
+    } else {
+      taskRemaining.tasks[e.target.parentElement.id].completed = false;
+      desc = e.target.nextElementSibling.innerHTML.replaceAll(/(<strike>|<\/strike>)/g, '');
+      e.target.nextElementSibling.innerHTML = desc;
+      taskRemaining.tasks[e.target.parentElement.id].description = desc;
+      save();
+    }
+  } else {
+    e.preventDefault();
+  }
+});
 
-  display = () => {
-    this.tasks.forEach((task, index) => {
-      const taskItem = document.createElement('li');
-      taskItem.classList.add('each-task');
-      taskItem.id = index;
-      taskItem.innerHTML = `
-      <input type="checkbox" id="task-${task.index}" name="task-${task.index}" ${task.completed ? 'checked' : 'unchecked'}>
-      <p contenteditable="true" class="task-to-be-done">${task.description}</p>
-      `;
-      container.appendChild(taskItem);
-    });
-  };
+window.addEventListener('load', () => {
+  retrieve();
+  taskRemaining.display();
+});
 
-  deleteCompleted = () => {
-    this.tasks = this.tasks.filter((task) => task.completed === false);
-    container.innerHTML = '';
-  };
+clearChecked.addEventListener('click', () => {
+  taskRemaining.deleteCompleted();
+  taskRemaining.updateIndex();
+  save();
+  taskRemaining.display();
+});
 
-  updateIndex = () => {
-    this.tasks.forEach((task, index) => {
-      task.index = index + 1;
-    });
-  };
-
-  delete = (index) => {
-    this.tasks = this.tasks.filter((task) => task.index !== Number(index) + 1);
-  };
-}
-
-const taskRemaining = new Tasks();
-export default taskRemaining;
+container.addEventListener('click', (e) => {
+  if (e.target.className === 'bin') {
+    taskRemaining.delete(e.target.parentElement.id);
+    taskRemaining.init();
+    taskRemaining.updateIndex();
+    save();
+    taskRemaining.display();
+  } else if (e.target.className === 'task-to-be-done') {
+    e.preventDefault();
+  }
+});
