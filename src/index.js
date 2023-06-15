7ec378f3f2a061b2cbaa3cae590ac77ac16fe5e6
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-06-15 11:33:01 +0300

    Implement status updates, clear completed, and local storage functionality

--- src/index.js
+++ src/index.js
@@ -1,90 +1,33 @@
-// eslint-disable-next-line import/named
-import './style.css';
-import add from './modules/add.js';
-import taskRemaining from './modules/taskFunctions.js';
-import {
-  addButton, taskInput, container, clearChecked,
-} from './modules/taskClass.js';
-import { save, retrieve } from './modules/storage.js';
+import add from './add.js';
+import { save, retrieve } from './storage.js';
+import { clearChecked, container } from './taskClass.js';
+import taskRemaining from './taskFunctions.js';
 
-taskInput.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter') {
-    if (taskInput.value === '') {
-      e.preventDefault();
-    } else {
-      const task = add(e);
-      taskRemaining.add(task);
-      taskRemaining.init();
-      save();
-      taskRemaining.display();
-    }
-  }
+// Add event listener to the checkbox
+container.addEventListener('change', (event) => {
+  const taskId = event.target.id.split('-')[1];
+  const task = taskRemaining.tasks[taskId - 1];
+  task.completed = event.target.checked;
+  save(); 
 });
 
-addButton.addEventListener('click', (e) => {
-  if (taskInput.value === '') {
-    e.preventDefault();
-  } else {
-    const task = add(e);
-    taskRemaining.add(task);
-    taskRemaining.init();
-    save();
-    taskRemaining.display();
-  }
-});
-
-container.addEventListener('keypress', (e) => {
-  if (e.target.className === 'task-to-be-done' && e.key === 'Enter') {
-    if (e.target.textContent) {
-      e.preventDefault();
-      taskRemaining.update(e.target.textContent, e.target.parentElement.id);
-      save();
-    } else {
-      e.preventDefault();
-    }
-  }
-});
-
-container.addEventListener('change', (e) => {
-  let desc = taskRemaining.tasks[e.target.parentElement.id].description;
-  if (e.target.type === 'checkbox') {
-    if (e.target.checked) {
-      taskRemaining.tasks[e.target.parentElement.id].completed = true;
-      e.target.nextElementSibling.innerHTML = `<strike>${desc}</strike>`;
-      taskRemaining.tasks[e.target.parentElement.id].description = `<strike>${desc}</strike>`;
-      save();
-    } else {
-      taskRemaining.tasks[e.target.parentElement.id].completed = false;
-      desc = e.target.nextElementSibling.innerHTML.replaceAll(/(<strike>|<\/strike>)/g, '');
-      e.target.nextElementSibling.innerHTML = desc;
-      taskRemaining.tasks[e.target.parentElement.id].description = desc;
-      save();
-    }
-  } else {
-    e.preventDefault();
-  }
-});
-
-window.addEventListener('load', () => {
-  retrieve();
-  taskRemaining.display();
-});
-
+// Implement the "Clear all completed" button functionality
 clearChecked.addEventListener('click', () => {
   taskRemaining.deleteCompleted();
-  taskRemaining.updateIndex();
-  save();
-  taskRemaining.display();
+  save(); 
 });
 
-container.addEventListener('click', (e) => {
-  if (e.target.className === 'bin') {
-    taskRemaining.delete(e.target.parentElement.id);
-    taskRemaining.init();
-    taskRemaining.updateIndex();
-    save();
-    taskRemaining.display();
-  } else if (e.target.className === 'task-to-be-done') {
-    e.preventDefault();
-  }
+// Load tasks from local storage on page load
+window.addEventListener('DOMContentLoaded', () => {
+  retrieve(); 
+  taskRemaining.display(); 
+});
+
+// Add event listener to the "Add" button
+addButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const task = add(event);
+  taskRemaining.add(task);
+  taskRemaining.display(); 
+  save(); 
 });
